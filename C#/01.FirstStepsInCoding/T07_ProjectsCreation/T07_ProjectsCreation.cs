67dae6e91ac7efe4f7c1c8fc8ea3e873f3343d98
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 17:35:17 +0300

    Update T07_ProjectsCreation.cs

--- C#/01.FirstStepsInCoding/T07_ProjectsCreation/T07_ProjectsCreation.cs
+++ C#/01.FirstStepsInCoding/T07_ProjectsCreation/T07_ProjectsCreation.cs
@@ -1,1 +1,17 @@
+using System;
+namespace Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfTheArchitect = Console.ReadLine();
+            int numbersOfProjects = int.Parse(Console.ReadLine());
+            int individualProjectTime = 3;
+            int timeForAllProjects = numbersOfProjects * individualProjectTime;
+
+            Console.WriteLine($"The architect {nameOfTheArchitect} will need {timeForAllProjects} hours to complete {numbersOfProjects} project/s.");
+        }
+    }
+}
 
