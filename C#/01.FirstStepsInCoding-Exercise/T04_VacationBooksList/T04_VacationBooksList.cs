79e4cff3b633347261daf8499da827c5d5d93cb6
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 22:14:32 +0300

    Update T04_VacationBooksList.cs

--- C#/01.FirstStepsInCoding-Exercise/T04_VacationBooksList/T04_VacationBooksList.cs
+++ C#/01.FirstStepsInCoding-Exercise/T04_VacationBooksList/T04_VacationBooksList.cs
@@ -1,1 +1,18 @@
+using System;
+namespace Vacation_Books_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int page = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            int totalHour = page / pagesPerHour;
+            int hoursPerDay = totalHour / days;
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
 
