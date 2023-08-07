c18d291763df5bb48700bd9af18046cf30955a17
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 17:41:59 +0300

    Update T09_YardGreening.cs

--- C#/01.FirstStepsInCoding/T09_YardGreening/T09_YardGreening.cs
+++ C#/01.FirstStepsInCoding/T09_YardGreening/T09_YardGreening.cs
@@ -1,1 +1,19 @@
+using System;
+namespace Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareMetre = double.Parse(Console.ReadLine());
+            double squareMetrePrice = 7.61;
+            double finalPrice = squareMetre * squareMetrePrice;
+            double discount = finalPrice * 0.18;
+            double totalPrice = finalPrice - discount;
+
+            Console.WriteLine($"The final price is: {totalPrice} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
 
