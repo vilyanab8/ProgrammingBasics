e4be56a4aca006cc14d25a3dc16118cb35457575
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 22:18:08 +0300

    Update T06_Repainting.cs

--- C#/01.FirstStepsInCoding-Exercise/T06_Repainting/T06_Repainting.cs
+++ C#/01.FirstStepsInCoding-Exercise/T06_Repainting/T06_Repainting.cs
@@ -1,1 +1,27 @@
+using System;
+namespace Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylonQuantity = int.Parse(Console.ReadLine());
+            double paintQuantity = double.Parse(Console.ReadLine());
+            int thinnerLiters = int.Parse(Console.ReadLine());
+            int worksHours = int.Parse(Console.ReadLine());
+
+            paintQuantity = paintQuantity + paintQuantity * 0.10;
+            nylonQuantity = nylonQuantity + 2;
+            double nylonPrice = nylonQuantity * 1.5;
+            double paintPrice = paintQuantity * 14.50;
+            double thinnerPrice = thinnerLiters * 5.00;
+            double materialsSum = nylonPrice + paintPrice + thinnerPrice + 0.40;
+
+            double pricePerHours = materialsSum * 0.30;
+            double worksSum = worksHours * pricePerHours;
+            double totalSum = worksSum + materialsSum;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
 
