c0185da6389e67d81214c838999d922d4550d7c0
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 22:21:02 +0300

    Update T08_BasketballEquipment.cs

--- C#/01.FirstStepsInCoding-Exercise/T08_BasketballEquipment/T08_BasketballEquipment.cs
+++ C#/01.FirstStepsInCoding-Exercise/T08_BasketballEquipment/T08_BasketballEquipment.cs
@@ -1,1 +1,20 @@
+using System;
+namespace Basketball_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double taxPerYear = double.Parse(Console.ReadLine());
+
+            double shoesPrice = taxPerYear - taxPerYear * 0.4;
+            double outfitPrice = shoesPrice - shoesPrice * 0.2;
+            double ballPrice = outfitPrice / 4;
+            double accPrice = ballPrice / 5;
+
+            double totalSum = shoesPrice + taxPerYear + outfitPrice + ballPrice + accPrice;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
 
