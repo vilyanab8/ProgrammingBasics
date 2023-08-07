ca2b2c6cf2021530bd28609f22d4c0cbce58ae2f
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 22:16:29 +0300

    Update T05_SuppliesForSchool.cs

--- C#/01.FirstStepsInCoding-Exercise/T05_SuppliesForSchool/T05_SuppliesForSchool.cs
+++ C#/01.FirstStepsInCoding-Exercise/T05_SuppliesForSchool/T05_SuppliesForSchool.cs
@@ -1,1 +1,23 @@
+using System;
+namespace Supplies_for_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hPackets = int.Parse(Console.ReadLine());
+            int mPackets = int.Parse(Console.ReadLine());
+            int prLiters = int.Parse(Console.ReadLine());
+            int percentSale = int.Parse(Console.ReadLine());
+
+            double hPrice = hPackets * 5.80;
+            double mPrice = mPackets * 7.20;
+            double prPrice = prLiters * 1.20;
+            double totalPrice = hPrice + mPrice + prPrice;
+            double totalSale = ((totalPrice * percentSale) / 100);
+            double totalPriceSale = totalPrice - totalSale;
+            Console.WriteLine(totalPriceSale);
+        }
+    }
+}
 
