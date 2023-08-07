1c201bad11ee531cc526b1fcd55fa751760035bf
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 17:39:37 +0300

    Update T08_PetShop.cs

--- C#/01.FirstStepsInCoding/T08_PetShop/T08_PetShop.cs
+++ C#/01.FirstStepsInCoding/T08_PetShop/T08_PetShop.cs
@@ -1,1 +1,20 @@
+using System;
+namespace Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFoodCount = int.Parse(Console.ReadLine());
+            int catFoodCount = int.Parse(Console.ReadLine());
+            double singleDogFoodPrice = 2.5;
+            double singleCatFoodPrice = 4;
+            double totalPriceForDogFood = dogFoodCount * singleDogFoodPrice;
+            double totalPriceForCatFood = catFoodCount * singleCatFoodPrice;
+            double totalPrice = totalPriceForDogFood + totalPriceForCatFood;
+
+            Console.WriteLine($"{totalPrice} lv.");
+        }
+    }
+}
 
