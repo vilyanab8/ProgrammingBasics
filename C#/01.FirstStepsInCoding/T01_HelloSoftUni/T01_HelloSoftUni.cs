006d6cdeb716af6dd019cd14458caad31ea72a04
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 17:08:23 +0300

    Update T01_HelloSoftUni.cs

--- C#/01.FirstStepsInCoding/T01_HelloSoftUni/T01_HelloSoftUni.cs
+++ C#/01.FirstStepsInCoding/T01_HelloSoftUni/T01_HelloSoftUni.cs
@@ -1,1 +1,13 @@
+using System;
+
+namespace Hello_SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
 
