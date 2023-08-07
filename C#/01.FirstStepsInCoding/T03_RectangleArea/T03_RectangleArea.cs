a4336c5920985bfafd13d68d4a0193c1c9fee7e4
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 17:21:12 +0300

    Update T03_RectangleArea.cs

--- C#/01.FirstStepsInCoding/T03_RectangleArea/T03_RectangleArea.cs
+++ C#/01.FirstStepsInCoding/T03_RectangleArea/T03_RectangleArea.cs
@@ -1,1 +1,16 @@
+using System;
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int area = a * b;
+            Console.WriteLine(area);
+        }
+    }
+}
 
