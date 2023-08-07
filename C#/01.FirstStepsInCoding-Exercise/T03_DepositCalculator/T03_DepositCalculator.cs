ed6f773cf81669abcc7e54b8d344da3b00e42cae
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 22:13:01 +0300

    Update T03_DepositCalculator.cs

--- C#/01.FirstStepsInCoding-Exercise/T03_DepositCalculator/T03_DepositCalculator.cs
+++ C#/01.FirstStepsInCoding-Exercise/T03_DepositCalculator/T03_DepositCalculator.cs
@@ -1,1 +1,18 @@
+using System;
+namespace Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositSum = double.Parse(Console.ReadLine());
+            int depositTerm = int.Parse(Console.ReadLine());
+            double percentPerYear = double.Parse(Console.ReadLine());
+
+            double sum = ((depositSum * percentPerYear) / 1200);
+            double totalSum = depositSum + depositTerm * sum;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
 
