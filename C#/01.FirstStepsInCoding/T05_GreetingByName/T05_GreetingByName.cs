ace54162cf85e27c5ebff19f2410252cf2e507da
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 17:25:06 +0300

    Update T05_GreetingByName.cs

--- C#/01.FirstStepsInCoding/T05_GreetingByName/T05_GreetingByName.cs
+++ C#/01.FirstStepsInCoding/T05_GreetingByName/T05_GreetingByName.cs
@@ -1,1 +1,13 @@
+using System;
+namespace Greeting_by_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
 
