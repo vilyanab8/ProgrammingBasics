92dc606511536ef751dc2127c99f638df1c659c7
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 22:25:54 +0300

    Update T09_FishTank.cs

--- C#/01.FirstStepsInCoding-Exercise/T09_FishTank/T09_FishTank.cs
+++ C#/01.FirstStepsInCoding-Exercise/T09_FishTank/T09_FishTank.cs
@@ -1,1 +1,23 @@
+using System;
+namespace Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            double percent = double.Parse(Console.ReadLine());
+
+            int volume = lenght * width * height;
+            double totalLiters = volume * 0.001;
+            double newPercent = percent * 0.01;
+
+            double result = totalLiters * (1 - newPercent);
+            Console.WriteLine($"{result:F3}");
+        }
+    }
+}
 
