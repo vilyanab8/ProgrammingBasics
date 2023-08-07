93ac207dfeb68978a33551ab07526b5bb9b362c7
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-07 22:19:40 +0300

    Update T07_FoodDelivery.cs

--- C#/01.FirstStepsInCoding-Exercise/T07_FoodDelivery/T07_FoodDelivery.cs
+++ C#/01.FirstStepsInCoding-Exercise/T07_FoodDelivery/T07_FoodDelivery.cs
@@ -1,1 +1,22 @@
+using System;
+namespace Food_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countChickenMenu = int.Parse(Console.ReadLine());
+            int countFishMenu = int.Parse(Console.ReadLine());
+            int countVegetarianMenu = int.Parse(Console.ReadLine());
+
+            double chickenMenuPrice = countChickenMenu * 10.35;
+            double fishMenuPrice = countFishMenu * 12.40;
+            double vegetarianMenuPrice = countVegetarianMenu * 8.15;
+            double menuSum = chickenMenuPrice + fishMenuPrice + vegetarianMenuPrice;
+            double desert = menuSum * 0.2;
+            double totalSum = menuSum + desert + 2.50;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
 
