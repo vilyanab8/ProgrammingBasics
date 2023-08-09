1f134bbd4d84ed38a8ef38873f04599749a65682
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 17:26:31 +0300

    Update T03_CelsiusToFahrenheit.cs

--- C#/01.FirstStepsInCoding-MoreExercise/T03_CelsiusToFahrenheit/T03_CelsiusToFahrenheit.cs
+++ C#/01.FirstStepsInCoding-MoreExercise/T03_CelsiusToFahrenheit/T03_CelsiusToFahrenheit.cs
@@ -1,1 +1,14 @@
+using System;
+namespace Celsius_to_Farenhait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius = double.Parse(Console.ReadLine());
+            double farenhait = (celcius * 9)/5 + 32;
+            Console.WriteLine("{0:f2}",farenhait);
+        }
+    }
+}
 
