970be4ffb108f81c671beba189f3e41be3e4ab44
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 18:20:59 +0300

    Update T07_AreaOfFigures.cs

--- C#/02.ConditionalStatements/T07_AreaOfFigures/T07_AreaOfFigures.cs
+++ C#/02.ConditionalStatements/T07_AreaOfFigures/T07_AreaOfFigures.cs
@@ -1,1 +1,39 @@
+using System;
+namespace Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figureName = Console.ReadLine();
+
+            if (figureName == "square")
+            {
+                double figureSide = double.Parse(Console.ReadLine());
+                double resultOfSides = figureSide * figureSide;
+                Console.WriteLine("{0:f3}",resultOfSides);
+            }
+            else if (figureName == "rectangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                double finalResult = side1 * side2;
+                Console.WriteLine("{0:f3}", finalResult);
+            }
+            else if (figureName == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double finalResult = radius * radius * Math.PI;
+                Console.WriteLine("{0:f3}", finalResult);
+            }
+            else if (figureName == "triangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                double finalResult = side1 * side2 / 2;
+                Console.WriteLine("{0:f3}", finalResult);
+            }
+        }
+    }
+}
 
