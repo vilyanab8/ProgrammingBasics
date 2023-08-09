295716b421555ac0f6ce462ef0b31f1daeec94a2
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 17:28:37 +0300

    Update T04_VegetableMarket.cs

--- C#/01.FirstStepsInCoding-MoreExercise/T04_VegetableMarket/T04_VegetableMarket.cs
+++ C#/01.FirstStepsInCoding-MoreExercise/T04_VegetableMarket/T04_VegetableMarket.cs
@@ -1,1 +1,21 @@
+using System;
+namespace Vegetable_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegPrice = double.Parse(Console.ReadLine());
+            double fruitPrice = double.Parse(Console.ReadLine());
+            int totalKgVeg = int.Parse(Console.ReadLine());
+            int totalKgFruit = int.Parse(Console.ReadLine());
+
+            double sum = vegPrice * totalKgVeg;
+            double sum1 = fruitPrice * totalKgFruit;
+            double totalSum = sum + sum1;
+            double endSum = totalSum / 1.94;
+            Console.WriteLine("{0:f2}",endSum);
+        }
+    }
+}
 
