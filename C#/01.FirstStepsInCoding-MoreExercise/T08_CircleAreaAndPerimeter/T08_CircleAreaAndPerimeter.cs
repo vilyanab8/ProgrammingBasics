9ce0a52798dca96a27834abc962c5e89bfa15e57
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 17:34:06 +0300

    Update T08_CircleAreaAndPerimeter.cs

--- C#/01.FirstStepsInCoding-MoreExercise/T08_CircleAreaAndPerimeter/T08_CircleAreaAndPerimeter.cs
+++ C#/01.FirstStepsInCoding-MoreExercise/T08_CircleAreaAndPerimeter/T08_CircleAreaAndPerimeter.cs
@@ -1,1 +1,6 @@
+double r = double.Parse(Console.ReadLine());
+double area = Math.PI * r * r;
+double parameter = 2 * Math.PI * r;
+Console.WriteLine($"{area:f2}");
+Console.WriteLine($"{parameter:f2}");
 
