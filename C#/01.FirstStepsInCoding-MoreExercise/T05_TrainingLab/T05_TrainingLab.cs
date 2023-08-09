5f62a94498a5ac19b6f8b9cd02b1b94bbbff7cf3
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 17:30:00 +0300

    Update T05_TrainingLab.cs

--- C#/01.FirstStepsInCoding-MoreExercise/T05_TrainingLab/T05_TrainingLab.cs
+++ C#/01.FirstStepsInCoding-MoreExercise/T05_TrainingLab/T05_TrainingLab.cs
@@ -1,1 +1,11 @@
+double w = double.Parse(Console.ReadLine());
+double h = double.Parse(Console.ReadLine());
 
+double w1 = w * 100;
+double h1 = h * 100;
+double metWithout = h1 - 100;
+double tables = Math.Round(metWithout / 70);
+double col = Math.Round(w1 / 120);
+
+double total = col * tables - 3;
+Console.WriteLine(total);
