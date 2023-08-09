e7226edbb6f346fb787e9f88fb07facdd8a119f9
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 18:15:04 +0300

    Update T04_PasswordGuess.cs

--- C#/02.ConditionalStatements/T04_PasswordGuess/T04_PasswordGuess.cs
+++ C#/02.ConditionalStatements/T04_PasswordGuess/T04_PasswordGuess.cs
@@ -1,1 +1,22 @@
+using System;
+namespace Password_Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "s3cr3t!P@ssw0rd";
+            string passwordFromTheUser = Console.ReadLine();
+
+            if (passwordFromTheUser == password)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
 
