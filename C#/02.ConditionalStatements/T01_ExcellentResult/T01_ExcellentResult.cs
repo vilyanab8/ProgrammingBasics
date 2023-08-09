9e75a08837a969326a3d3dbdd1eccb82a760c877
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 18:11:04 +0300

    Update T01_ExcellentResult.cs

--- C#/02.ConditionalStatements/T01_ExcellentResult/T01_ExcellentResult.cs
+++ C#/02.ConditionalStatements/T01_ExcellentResult/T01_ExcellentResult.cs
@@ -1,1 +1,17 @@
+using System;
+namespace Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentScore = double.Parse(Console.ReadLine());
+
+            if (currentScore >= 5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
 
