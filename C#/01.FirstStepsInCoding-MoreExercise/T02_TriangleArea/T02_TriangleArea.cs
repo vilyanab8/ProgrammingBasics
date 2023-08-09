c4c61846d6f96e6cc9c07e40a946489a380a753e
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 17:25:19 +0300

    Update T02_TriangleArea.cs

--- C#/01.FirstStepsInCoding-MoreExercise/T02_TriangleArea/T02_TriangleArea.cs
+++ C#/01.FirstStepsInCoding-MoreExercise/T02_TriangleArea/T02_TriangleArea.cs
@@ -1,1 +1,16 @@
+using System;
+namespace Triangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double area = side * h / 2;
+            Console.WriteLine("{0:f2}",area);
+        }
+    }
+}
 
