76f40cb58dd92be266edecb328f2d073ae96c48e
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 17:23:43 +0300

    Update T01_TrapezoidArea.cs

--- C#/01.FirstStepsInCoding-MoreExercise/T01_TrapezoidArea/T01_TrapezoidArea.cs
+++ C#/01.FirstStepsInCoding-MoreExercise/T01_TrapezoidArea/T01_TrapezoidArea.cs
@@ -1,1 +1,17 @@
+using System;
+namespace Trapezoid_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b1 = double.Parse(Console.ReadLine());
+            double b2 = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double area = (b1 + b2) * h / 2;
+            Console.WriteLine("{0:f2}",area);
+        }
+    }
+}
 
