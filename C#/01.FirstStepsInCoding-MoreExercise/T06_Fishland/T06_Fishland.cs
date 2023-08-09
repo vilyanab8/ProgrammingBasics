1f75cd2e7fe0925669e6c262af49768a15b02e3f
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 17:31:19 +0300

    Update T06_Fishland.cs

--- C#/01.FirstStepsInCoding-MoreExercise/T06_Fishland/T06_Fishland.cs
+++ C#/01.FirstStepsInCoding-MoreExercise/T06_Fishland/T06_Fishland.cs
@@ -1,1 +1,17 @@
+double mackerelPrice = double.Parse(Console.ReadLine());
+double sprinklePrice = double.Parse(Console.ReadLine());
+double bonitoKilograms = double.Parse(Console.ReadLine());
+double scadKilograms = double.Parse(Console.ReadLine());
+double clamKilograms = double.Parse(Console.ReadLine());
+
+double bonitoPrice = mackerelPrice + mackerelPrice * 0.60;
+double bonitoSum = bonitoKilograms * bonitoPrice;
+
+double scadPrice = sprinklePrice + sprinklePrice * 0.80;
+double scadSum = scadKilograms * scadPrice;
+
+double clamSum = clamKilograms * 7.50;
+
+double subTotal = bonitoSum + scadSum + clamSum;
+Console.WriteLine($"{subTotal:f2}");
 
