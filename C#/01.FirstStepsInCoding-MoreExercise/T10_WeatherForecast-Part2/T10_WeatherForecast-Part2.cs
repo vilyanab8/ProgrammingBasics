9e69497d73ecccc4ae2b83567b56d34e13a52611
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-08-09 17:39:37 +0300

    Update T10_WeatherForecast-Part2.cs

--- C#/01.FirstStepsInCoding-MoreExercise/T10_WeatherForecast-Part2/T10_WeatherForecast-Part2.cs
+++ C#/01.FirstStepsInCoding-MoreExercise/T10_WeatherForecast-Part2/T10_WeatherForecast-Part2.cs
@@ -1,1 +1,26 @@
+double degree = double.Parse(Console.ReadLine());
 
+if (degree >= 5.00 && degree <= 11.9)
+{
+    Console.WriteLine("Cold");
+}
+else if (degree >= 12.00 && degree <= 14.9)
+{
+    Console.WriteLine("Cool");
+}
+else if (degree >= 15.00 && degree <= 20.00)
+{
+    Console.WriteLine("Mild");
+}
+else if (degree >= 20.1 && degree <= 25.9)
+{
+    Console.WriteLine("Warm");
+}
+else if (degree >= 26.00 && degree <= 35.00)
+{
+    Console.WriteLine("Hot");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
