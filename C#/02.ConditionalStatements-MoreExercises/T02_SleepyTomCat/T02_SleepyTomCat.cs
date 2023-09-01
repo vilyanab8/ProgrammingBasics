cf3b59fbc718af6968f1772ab7cfde10ed9d4c56
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-01 15:39:10 +0300

    100/100 judge

--- C#/02.ConditionalStatements-MoreExercises/T02_SleepyTomCat/T02_SleepyTomCat.cs
+++ C#/02.ConditionalStatements-MoreExercises/T02_SleepyTomCat/T02_SleepyTomCat.cs
@@ -1,1 +1,16 @@
-
+int breakDays = int.Parse(Console.ReadLine());
+int norma = 30000;
+int workDays = 365 - breakDays;
+int timeIgra = workDays * 63 + breakDays * 127;
+int mins = Math.Abs(norma - timeIgra) % 60;
+int hours = Math.Abs(norma - timeIgra) / 60;
+if (timeIgra >= norma)
+{
+    Console.WriteLine("Tom will run away");
+    Console.WriteLine("{0} hours and {1} minutes more for play", hours, mins);
+}
+else
+{
+    Console.WriteLine("Tom sleeps well");
+    Console.WriteLine("{0} hours and {1} minutes less for play", hours, mins);
+}
