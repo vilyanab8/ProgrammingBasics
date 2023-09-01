3aea79344919b4a91ca98f1b6ca826055e01dfce
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-01 15:45:23 +0300

    100/100 judge

--- C#/02.ConditionalStatements-MoreExercises/T05_Pets/T05_Pets.cs
+++ C#/02.ConditionalStatements-MoreExercises/T05_Pets/T05_Pets.cs
@@ -1,1 +1,21 @@
+int days = int.Parse(Console.ReadLine());
+int kgFood = int.Parse(Console.ReadLine());
+double dogFood = double.Parse(Console.ReadLine());
+double catFood = double.Parse(Console.ReadLine());
+double turtleFood = double.Parse(Console.ReadLine());
+
+double needDogFood = days * dogFood;
+double needCatFood = days * catFood;
+double needTurtleFood = days * (turtleFood/1000);
+
+double totalFood = needDogFood + needCatFood + needTurtleFood;
+
+if (totalFood >= kgFood)
+{
+    Console.WriteLine($"{Math.Ceiling(totalFood-kgFood)} more kilos of food are needed.");
+}
+else if (totalFood < kgFood)
+{
+    Console.WriteLine($"{Math.Floor(kgFood-totalFood)} kilos of food left.");
+}
 
