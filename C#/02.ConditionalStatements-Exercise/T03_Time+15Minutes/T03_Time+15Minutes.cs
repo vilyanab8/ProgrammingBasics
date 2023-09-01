9a59e9e28f7dd48f465508b65d1cbadd7d910872
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-01 15:03:04 +0300

    100/100 judge

--- C#/02.ConditionalStatements-Exercise/T03_Time+15Minutes/T03_Time+15Minutes.cs
+++ C#/02.ConditionalStatements-Exercise/T03_Time+15Minutes/T03_Time+15Minutes.cs
@@ -1,1 +1,33 @@
+using System;
+namespace Time___15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+
+            min = min + (h * 60);
+            int minAfter15Min = min + 15;
+            int hAfter = minAfter15Min / 60;
+            int mAfter = minAfter15Min % 60;
+
+            if (hAfter > 23)
+            {
+                hAfter = 0;
+            }
+
+            if (mAfter < 10)
+            {
+                Console.WriteLine($"{hAfter}:0{mAfter}");
+            }
+
+            else
+            {
+                Console.WriteLine($"{hAfter}:{mAfter}");
+            }
+        }
+    }
+}
 
