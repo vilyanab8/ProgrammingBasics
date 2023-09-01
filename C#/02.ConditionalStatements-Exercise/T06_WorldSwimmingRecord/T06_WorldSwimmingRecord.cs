be3e4efaeb37c925aab36af7c236833835b58825
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-01 15:09:41 +0300

    100/100 judge

--- C#/02.ConditionalStatements-Exercise/T06_WorldSwimmingRecord/T06_WorldSwimmingRecord.cs
+++ C#/02.ConditionalStatements-Exercise/T06_WorldSwimmingRecord/T06_WorldSwimmingRecord.cs
@@ -1,1 +1,29 @@
+using System;
+namespace World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double secRecord = double.Parse(Console.ReadLine());
+            double metresDistance = double.Parse(Console.ReadLine());
+            double timeInSecFor1Metre = double.Parse(Console.ReadLine());
+
+            double time = metresDistance * timeInSecFor1Metre;
+            double every15Metres = Math.Floor(metresDistance / 15) * 12.5;
+            double totalTime = time + every15Metres;
+            double missedTime = totalTime - secRecord; 
+
+            if (totalTime < secRecord)
+            {
+                Console.WriteLine($" Yes, he succeeded! The new world record is {totalTime:F2} seconds.");
+            }
+            
+            else if (totalTime > secRecord)
+            {
+                Console.WriteLine($"No, he failed! He was {missedTime:F2} seconds slower.");
+            }
+        }
+    }
+}
 
