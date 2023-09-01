17cedb6e2174fb3a1a53b25df7360089f02205fd
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-01 15:07:50 +0300

    100/100 judge

--- C#/02.ConditionalStatements-Exercise/T05_GodzillaVs.Kong/T05_GodzillaVs.Kong.cs
+++ C#/02.ConditionalStatements-Exercise/T05_GodzillaVs.Kong/T05_GodzillaVs.Kong.cs
@@ -1,1 +1,37 @@
+using System;
+namespace Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statists = int.Parse(Console.ReadLine());
+            double clothingPrice = double.Parse(Console.ReadLine());
+
+            double decor = budget * 0.10;
+            double clothing = statists * clothingPrice;
+
+            if (statists > 150)
+            {
+                clothing = clothing - clothing * 0.10;
+            }
+
+            double neededMoney = clothing + decor;
+            double difference = budget - neededMoney;
+
+            if (difference >= 0)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {difference:f2} leva left.");
+            }
+
+            else
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {Math.Abs(difference):f2} leva more.");
+            }
+        }
+    }
+}
 
