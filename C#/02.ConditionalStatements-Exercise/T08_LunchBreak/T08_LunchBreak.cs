676af9f9388366e3746d8deccb61de9019b289f8
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-01 15:16:22 +0300

    100/100 judge

--- C#/02.ConditionalStatements-Exercise/T08_LunchBreak/T08_LunchBreak.cs
+++ C#/02.ConditionalStatements-Exercise/T08_LunchBreak/T08_LunchBreak.cs
@@ -1,1 +1,26 @@
+using System;
+namespace Lunch_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seriesName = Console.ReadLine();
+            int seriesTime = int.Parse(Console.ReadLine());
+            int lunchBreak = int.Parse(Console.ReadLine());
+
+            double timeForSeries = lunchBreak * 5 / 8.0;
+
+            if (timeForSeries >= seriesTime)
+            {
+                Console.WriteLine($"You have enough time to watch {seriesName} and left with {Math.Ceiling(timeForSeries - seriesTime)} minutes free time.");
+            }
+
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {seriesName}, you need {Math.Ceiling(seriesTime - timeForSeries)} more minutes.");
+            }
+        }
+    }
+}
 
