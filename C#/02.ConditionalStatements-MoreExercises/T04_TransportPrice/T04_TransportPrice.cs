a8620a0b8a422b84cd3447a533d03de0130c61ba
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-01 15:43:29 +0300

    100/100 judge

--- C#/02.ConditionalStatements-MoreExercises/T04_TransportPrice/T04_TransportPrice.cs
+++ C#/02.ConditionalStatements-MoreExercises/T04_TransportPrice/T04_TransportPrice.cs
@@ -1,1 +1,36 @@
+int n = int.Parse(Console.ReadLine());
+string dayOrNight = Console.ReadLine();
+double totalPrice = 0.0;
 
+if (n < 20)
+{
+    double startPrice = 0.7;
+	double price = 0.0;
+	if (dayOrNight == "day")
+	{
+		price = 0.79;
+	}
+	else if (dayOrNight == "night")
+	{
+		price = 0.9;
+	}
+	totalPrice = startPrice + n * price;
+}
+
+else if (n >= 20 && n < 100)
+{
+	if (dayOrNight == "day" || dayOrNight == "night")
+	{
+		totalPrice = n * 0.09;
+	}
+}
+
+else if (n >= 100)
+{
+	if (dayOrNight == "day" || dayOrNight == "night")
+	{
+        totalPrice = n * 0.06;
+    }
+}
+
+Console.WriteLine($"{totalPrice:f2}");
