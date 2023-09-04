afa16bf766fbeb1285601705c6b97c545e7908ad
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 14:22:46 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced/T10_InvalidNumber/T10_InvalidNumber.cs
+++ C#/03.ConditionalStatementsAdvanced/T10_InvalidNumber/T10_InvalidNumber.cs
@@ -1,1 +1,19 @@
+using System;
+
+namespace Invalid_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool isValid = number >= 100 && number <= 200 || number == 0;
+
+            if (!isValid)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
 
