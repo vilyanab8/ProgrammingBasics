1bbf8028bf2959eb798e68291abe6cedac680e6e
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 21:10:57 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced-MoreExercises/T04_CarToGo/T04_CarToGo.cs
+++ C#/03.ConditionalStatementsAdvanced-MoreExercises/T04_CarToGo/T04_CarToGo.cs
@@ -1,1 +1,46 @@
+double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+
+double price = 0.0;
+
+if (budget <= 100)
+{
+    Console.WriteLine("Economy class");
+	if (season == "Summer")
+	{
+		price = 0.35 * budget;
+		Console.WriteLine($"Cabrio - {(price):f2}");
+	}
+	else if (season == "Winter")
+	{
+		price = 0.65 * budget;
+		Console.WriteLine($"Jeep - {(price):f2}");
+	}
+}
+
+else if (budget > 100 && budget <= 500)
+{
+	Console.WriteLine("Compact class");
+	if (season == "Summer")
+	{
+		price = 0.45 * budget;
+		Console.WriteLine($"Cabrio - {(price):f2}");
+	}
+	else if (season == "Winter")
+	{
+		price = 0.8 * budget;
+		Console.WriteLine($"Jeep - {(price):f2}");
+	}
+}
+
+else if (budget > 500)
+{
+	Console.WriteLine("Luxury class");
+	if (season == "Summer" || season == "Winter")
+	{
+		price = 0.9 * budget;
+		Console.WriteLine($"Jeep - {(price):f2}");
+	}
+}
+
 
