96bc68e029805ebad188b635e2dc1ac3a4ef7fb6
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 21:25:59 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced-MoreExercises/T10_MultiplyBy2/T10_MultiplyBy2.cs
+++ C#/03.ConditionalStatementsAdvanced-MoreExercises/T10_MultiplyBy2/T10_MultiplyBy2.cs
@@ -1,1 +1,14 @@
+double num = double.Parse(Console.ReadLine());
 
+double result = 0;
+
+while (num >= 0)
+{
+    result = num * 2;
+    Console.WriteLine($"Result: {result:f2}");
+    num = double.Parse(Console.ReadLine());
+}
+if (num < 0)
+{
+    Console.WriteLine("Negative number!");
+}
