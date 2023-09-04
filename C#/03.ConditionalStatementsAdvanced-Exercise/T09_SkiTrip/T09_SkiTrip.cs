53e2d580c4ee0da06a02470b8fe385e7b37611e1
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 17:05:24 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced-Exercise/T09_SkiTrip/T09_SkiTrip.cs
+++ C#/03.ConditionalStatementsAdvanced-Exercise/T09_SkiTrip/T09_SkiTrip.cs
@@ -1,1 +1,79 @@
+using System;
+
+namespace Ski_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ddni = int.Parse(Console.ReadLine());
+            string pomeshtenie = Console.ReadLine();
+            string ochenka = Console.ReadLine();
+            double prestoi = 0;
+            double ostypka = 0;
+            double dni = ddni - 1;
+            if (pomeshtenie == "room for one person")
+            {
+                ostypka = dni * 18.00;
+
+
+            }
+            else if (pomeshtenie == "apartment")
+            {
+                prestoi = dni * 25.00;
+
+                if (dni > 0 && dni <= 10)
+                {
+                    ostypka = prestoi - (prestoi * 0.30);
+                }
+                else if (dni > 10 && dni <= 15)
+                {
+                    ostypka = prestoi - (prestoi * 0.35);
+                }
+                else if (dni > 15)
+                {
+                    ostypka = prestoi - (prestoi * 0.50);
+                }
+
+
+
+
+            }
+            else if (pomeshtenie == "president apartment")
+            {
+                prestoi = dni * 35.00;
+
+                if (dni > 0 && dni <= 10)
+                {
+                    ostypka = prestoi - (prestoi * 0.10);
+                }
+                else if (dni > 10 && dni <= 15)
+                {
+                    ostypka = prestoi - (prestoi * 0.15);
+                }
+                else if (dni > 15)
+                {
+                    ostypka = prestoi - (prestoi * 0.20);
+                }
+
+
+                //346
+
+            }
+
+
+            if (ochenka == "positive")
+            {
+                double bakshish = ostypka + (ostypka * 0.25);
+                Console.WriteLine($"{bakshish:F2}");
+            }
+            else if (ochenka == "negative")
+            {
+                double bakshish = ostypka - (ostypka * 0.10);
+                Console.WriteLine($"{bakshish:F2}");
+            }
+        }
+    }
+}
+
 
