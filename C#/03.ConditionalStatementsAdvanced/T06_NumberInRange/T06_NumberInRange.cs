bac96cba5aa9e639ff6fcf65bee7daa6935d93e0
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 14:14:31 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced/T06_NumberInRange/T06_NumberInRange.cs
+++ C#/03.ConditionalStatementsAdvanced/T06_NumberInRange/T06_NumberInRange.cs
@@ -1,1 +1,23 @@
+using System;
+
+namespace Number_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number >= -100 && number <= 100 && number != 0)
+            {
+                Console.WriteLine("Yes");
+            }
+
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
 
