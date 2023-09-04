71f1d8a21d995d0843f13bc9715520d5131199f0
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 14:03:38 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced/T04_PersonalTitles/T04_PersonalTitles.cs
+++ C#/03.ConditionalStatementsAdvanced/T04_PersonalTitles/T04_PersonalTitles.cs
@@ -1,1 +1,37 @@
+using System;
+
+namespace Personal_Titles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "f")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+            else
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+        }
+    }
+}
 
