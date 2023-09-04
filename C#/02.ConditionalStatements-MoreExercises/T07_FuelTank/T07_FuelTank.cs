5730ff5969c2797bde58690ff1c3a66460060b46
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 13:12:36 +0300

    100/100 judge

--- C#/02.ConditionalStatements-MoreExercises/T07_FuelTank/T07_FuelTank.cs
+++ C#/02.ConditionalStatements-MoreExercises/T07_FuelTank/T07_FuelTank.cs
@@ -1,1 +1,16 @@
+string text = Console.ReadLine();
+double n = double.Parse(Console.ReadLine());
 
+if (n >= 25)
+{
+    Console.WriteLine($"You have enough {text}.");
+}
+else if (n < 25)
+{
+    Console.WriteLine($"Fill your tank with {text}!");
+}
+
+else if ((text != "Diesel") || (text != "Gasoline") || (text != "Gas") || (text != "Kerosene"))
+{
+    Console.WriteLine("Invalid fuel!");
+}
