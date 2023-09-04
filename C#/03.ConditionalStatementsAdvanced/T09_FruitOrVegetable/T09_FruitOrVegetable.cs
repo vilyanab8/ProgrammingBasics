d6e67dcf605d0daafae5f3391b2f123ff1648f0b
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 14:20:47 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced/T09_FruitOrVegetable/T09_FruitOrVegetable.cs
+++ C#/03.ConditionalStatementsAdvanced/T09_FruitOrVegetable/T09_FruitOrVegetable.cs
@@ -1,1 +1,27 @@
+using System;
+
+namespace Fruit_or_Vegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            //banana, apple, kiwi, cherry, lemon и grapes
+            if (product == "banana" || product == "apple" || product == "kiwi" || product == "cherry" || product == "lemon" || product == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            //tomato, cucumber, pepper и carrot
+            else if (product == "tomato" || product == "cucumber" || product == "pepper" || product == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else if (product != "banana" || product != "apple" || product != "kiwi" || product != "cherry" || product != "lemon" || product != "grapes" || product != "tomato" || product != "cucumber" || product != "pepper" || product != "carrot")
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
 
