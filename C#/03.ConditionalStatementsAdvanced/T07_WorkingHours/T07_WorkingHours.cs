a936440b3977d19e4b02cf6b49587dd7897c13be
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 14:17:38 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced/T07_WorkingHours/T07_WorkingHours.cs
+++ C#/03.ConditionalStatementsAdvanced/T07_WorkingHours/T07_WorkingHours.cs
@@ -1,1 +1,26 @@
+using System;
+
+namespace Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Saturday")
+            {
+                if (hour >= 10 && hour <= 18)
+                {
+                    Console.WriteLine("open");
+                }
+                else
+                {
+                    Console.WriteLine("closed");
+                }
+            }
+        }
+    }
+}
 
