d4e67b5550c7fcf6ea24b3da2619016c932028c7
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 16:42:29 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced-Exercise/T01_Cinema/T01_Cinema.cs
+++ C#/03.ConditionalStatementsAdvanced-Exercise/T01_Cinema/T01_Cinema.cs
@@ -1,1 +1,32 @@
+using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeProjection = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            double income = 0.0;
+
+            if (typeProjection == "Premiere")
+            {
+                income = rows * columns * 12.00;
+            }
+            else if (typeProjection == "Normal")
+            {
+                income = rows * columns * 7.50;
+            }
+            else if (typeProjection == "Discount")
+            {
+                income = rows * columns * 5.00;
+            }
+
+            Console.WriteLine($"{income:f2} leva");
+        }
+    }
+}
 
