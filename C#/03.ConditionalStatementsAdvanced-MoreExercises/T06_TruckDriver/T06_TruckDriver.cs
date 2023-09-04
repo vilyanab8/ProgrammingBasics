70d2a126e2521fb2177ec8e789ce2b5155c440b8
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-04 21:15:11 +0300

    100/100 judge

--- C#/03.ConditionalStatementsAdvanced-MoreExercises/T06_TruckDriver/T06_TruckDriver.cs
+++ C#/03.ConditionalStatementsAdvanced-MoreExercises/T06_TruckDriver/T06_TruckDriver.cs
@@ -1,1 +1,62 @@
+string season = Console.ReadLine();
+double kilometers = double.Parse(Console.ReadLine());
+double money = 0;
+{
+    if (season == "Summer")
+    {
 
+        if (kilometers <= 5000)
+        {
+            money = (kilometers * 0.90);
+        }
+
+        else if (5000 < kilometers && kilometers <= 10000)
+        {
+            money = (kilometers * 1.10);
+        }
+        else
+        {
+            money = (kilometers * 1.45);
+        }
+    }
+    if (season == "Winter")
+    {
+
+        if (kilometers <= 5000)
+        {
+            money = (kilometers * 1.05);
+        }
+
+        else if (5000 < kilometers && kilometers <= 10000)
+        {
+            money = (kilometers * 1.25);
+        }
+        else
+        {
+            money = (kilometers * 1.45);
+        }
+    }
+
+    if (season == "Spring" || season == "Autumn")
+    {
+
+        if (kilometers <= 5000)
+        {
+            money = (kilometers * 0.75);
+        }
+
+        else if (5000 < kilometers && kilometers <= 10000)
+        {
+            money = (kilometers * 0.95);
+        }
+        else
+        {
+            money = (kilometers * 1.45);
+        }
+    }
+}
+money = (money * 4);
+money *= 0.9;
+
+Console.WriteLine($"{money:F2}");
+        
