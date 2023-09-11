25cead8d825d8116265d3ee89f2db37628fcf8ec
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-11 16:35:13 +0300

    100/100 judge

--- C#/04.ForLoop/T02_NumbersNTo1/T02_NumbersNTo1.cs
+++ C#/04.ForLoop/T02_NumbersNTo1/T02_NumbersNTo1.cs
@@ -1,1 +1,17 @@
+using System;
+
+namespace Numbers_N_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
 
