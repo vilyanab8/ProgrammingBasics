4c16eac19ef2830c200528d1426772323234410d
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-11 16:37:18 +0300

    100/100 judge

--- C#/04.ForLoop/T03_Numbers1ToNWithStep3/T03_Numbers1ToNWithStep3.cs
+++ C#/04.ForLoop/T03_Numbers1ToNWithStep3/T03_Numbers1ToNWithStep3.cs
@@ -1,1 +1,17 @@
+using System;
+
+namespace Numbers_from_1_to_N_through_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i+= 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
 
