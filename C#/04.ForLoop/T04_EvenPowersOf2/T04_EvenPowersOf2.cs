798722d1948455ecc24a9cad8ff46b2bcb57b28a
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-11 16:42:42 +0300

    100/100 judge

--- C#/04.ForLoop/T04_EvenPowersOf2/T04_EvenPowersOf2.cs
+++ C#/04.ForLoop/T04_EvenPowersOf2/T04_EvenPowersOf2.cs
@@ -1,1 +1,19 @@
+using System;
+
+namespace Even_Powers_of_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            for (int counter = 0; counter <= n; counter+= 2)
+            {
+                Console.WriteLine(num);
+                num *= 4;
+            }
+        }
+    }
+}
 
