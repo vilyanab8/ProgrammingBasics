7891e7ff587506007c6e17f7b94e0c69595df84c
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-09-11 16:45:29 +0300

    100/100 judge

--- C#/04.ForLoop/T05_CharacterSequence/T05_CharacterSequence.cs
+++ C#/04.ForLoop/T05_CharacterSequence/T05_CharacterSequence.cs
@@ -1,1 +1,18 @@
+using System;
+
+namespace Character_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char letter = input[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
 
