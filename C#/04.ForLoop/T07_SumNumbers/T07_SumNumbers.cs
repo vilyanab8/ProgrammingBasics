e7124ca305aec45dbb39cad9c88126f16d400e8f
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-07 20:10:57 +0300

    100/100 judge

--- C#/04.ForLoop/T07_SumNumbers/T07_SumNumbers.cs
+++ C#/04.ForLoop/T07_SumNumbers/T07_SumNumbers.cs
@@ -1,1 +1,20 @@
+using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            int sumOfNumbers = 0;
+            for (int counter = 0; counter < countOfInputs; counter++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                sumOfNumbers += currentNumber;
+            }
+            Console.WriteLine(sumOfNumbers);
+        }
+    }
+}
 
