275a91a0ba05132f26d0b2c891db2a3e835e218e
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-07 20:05:23 +0300

    100/100 judge

--- C#/04.ForLoop/T06_VowelsSum/T06_VowelsSum.cs
+++ C#/04.ForLoop/T06_VowelsSum/T06_VowelsSum.cs
@@ -1,1 +1,40 @@
+using System;
+
+namespace Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letter = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < letter.Length; i++)
+            {
+                if (letter[i] == 'a')
+                {
+                    sum += 1;
+                }
+                else if (letter[i] == 'e')
+                {
+                    sum += 2;
+                }
+                else if (letter[i] == 'i')
+                {
+                    sum += 3;
+                }
+                else if (letter[i] == 'o')
+                {
+                    sum += 4;
+                }
+                else if (letter[i] == 'u')
+                {
+                    sum += 5;
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
 
