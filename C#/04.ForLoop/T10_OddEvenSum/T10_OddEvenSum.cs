a82082fd877cff982b08efd825b5a66fb12d2987
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-07 20:21:18 +0300

    100/100 judge

--- C#/04.ForLoop/T10_OddEvenSum/T10_OddEvenSum.cs
+++ C#/04.ForLoop/T10_OddEvenSum/T10_OddEvenSum.cs
@@ -1,1 +1,39 @@
+using System;
+
+namespace Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int counter = 0; counter < countOfInputs; counter++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (counter % 2 == 0)
+                {
+                    evenSum += currentNumber;
+                }
+                else
+                {
+                    oddSum += currentNumber;
+                }
+
+            }
+            if (evenSum == oddSum)
+                {
+                    Console.WriteLine("Yes");
+                    Console.WriteLine($"Sum = {evenSum}");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                    Console.WriteLine($"Diff = {Math.Abs(evenSum - oddSum)}");
+                }
+            }
+        }
+    }
 
