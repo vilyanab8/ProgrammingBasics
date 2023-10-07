a46418c73fc8877bea2e0613aca1f7c59134904f
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-07 20:13:12 +0300

    100/100 judge

--- C#/04.ForLoop/T08_NumberSequence/T08_NumberSequence.cs
+++ C#/04.ForLoop/T08_NumberSequence/T08_NumberSequence.cs
@@ -1,1 +1,30 @@
+using System;
+
+namespace Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+            for (int counter = 0; counter < countOfInputs; counter++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
 
