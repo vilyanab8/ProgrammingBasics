56d7edc3d1d2b72b10bc438a166ad10a1876907f
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-07 20:18:00 +0300

    100/100 judge

--- C#/04.ForLoop/T09_LeftAndRightSum/T09_LeftAndRightSum.cs
+++ C#/04.ForLoop/T09_LeftAndRightSum/T09_LeftAndRightSum.cs
@@ -1,1 +1,36 @@
+using System;
+
+namespace Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int counter = 0; counter < 2 * countOfInputs; counter++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (counter < countOfInputs)
+                {
+                    leftSum += currentNumber;
+                }
+                else
+                {
+                    rightSum += currentNumber;
+                }
+            }
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftSum-rightSum)}");
+            }
+        }
+    }
+}
 
