0848e860cb6edf4501f452298d09cf2aca6631a5
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-07 22:31:05 +0300

    100/100 judge

--- C#/04.Forloop-MoreExercises/T01_BackToThePast/T01_BackToThePast.cs
+++ C#/04.Forloop-MoreExercises/T01_BackToThePast/T01_BackToThePast.cs
@@ -1,1 +1,26 @@
+double money = double.Parse(Console.ReadLine());
+int year = int.Parse(Console.ReadLine());
+double yearsOld = 17;
 
+for (int i = 1800; i <= year; i++)
+{
+    if (i % 2 == 0)
+    {
+        money = money - 12000;
+    }
+    else
+    {
+        yearsOld += 2;
+        money = money - (12000 + (50 * yearsOld));
+    }
+}
+
+
+if (money >= 0)
+{
+    Console.WriteLine($"Yes! He will live a carefree life and will have {money:f2} dollars left.");
+}
+else
+{
+    Console.WriteLine($"He will need {(Math.Abs(money)):f2} dollars to survive.");
+}
