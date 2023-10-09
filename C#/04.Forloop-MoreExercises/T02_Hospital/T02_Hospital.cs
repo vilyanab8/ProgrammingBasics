9638865f60df3eac6dff0366639bfbb4165fefbd
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-09 21:40:26 +0300

    100/100 judge

--- C#/04.Forloop-MoreExercises/T02_Hospital/T02_Hospital.cs
+++ C#/04.Forloop-MoreExercises/T02_Hospital/T02_Hospital.cs
@@ -1,1 +1,25 @@
+int period = int.Parse(Console.ReadLine());
+int treatedPatients = 0;
+int untreatedPatients = 0;
+int doctors = 7;
 
+for (int i = 1; i <= period; i++)
+{
+    int patients = int.Parse(Console.ReadLine());
+	if (i % 3 == 0 && untreatedPatients > treatedPatients)
+	{
+		doctors++;
+	}
+	if (patients > doctors)
+	{
+        untreatedPatients += patients - doctors;
+        treatedPatients += doctors;
+    }
+	else
+	{
+        treatedPatients += patients;
+    }
+}
+
+Console.WriteLine($"Treated patients: {treatedPatients}.");
+Console.WriteLine($"Untreated patients: {untreatedPatients}.");
