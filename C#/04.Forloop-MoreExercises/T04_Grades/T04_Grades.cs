e38bedb415422071c754b1d3f7611161c5db4147
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-09 22:56:26 +0300

    100/100 judge

--- C#/04.Forloop-MoreExercises/T04_Grades/T04_Grades.cs
+++ C#/04.Forloop-MoreExercises/T04_Grades/T04_Grades.cs
@@ -1,1 +1,35 @@
+int students = int.Parse(Console.ReadLine());
+int greaterThanFive = 0;
+int betweenFourAndFive = 0;
+int betweenThreeAndFour = 0;
+int PoorerThanThree = 0;
+double average = 0;
+
+for (int i = 0; i < students; i++)
+{
+    double gradeInput = double.Parse(Console.ReadLine());
+    if (gradeInput >= 5.00)
+    { 
+        greaterThanFive++;
+    }
+    else if (gradeInput >= 4.00 && gradeInput <= 4.99)
+    { 
+        betweenFourAndFive++;
+    }
+    else if (gradeInput >= 3.00 && gradeInput <= 3.99)
+    { 
+        betweenThreeAndFour++;
+    }
+    else if (gradeInput < 3.00)
+    { 
+        PoorerThanThree++;
+    }
+    average += gradeInput;
+}
+
+Console.WriteLine($"Top students: {((greaterThanFive / (double)students) * 100):f2}%");
+Console.WriteLine($"Between 4.00 and 4.99: {((betweenFourAndFive / (double)students) * 100):f2}%");
+Console.WriteLine($"Between 3.00 and 3.99: {((betweenThreeAndFour / (double)students) * 100):f2}%");
+Console.WriteLine($"Fail: {((PoorerThanThree / (double)students) * 100):f2}%");
+Console.WriteLine($"Average: {average / (double)students:f2}");
 
