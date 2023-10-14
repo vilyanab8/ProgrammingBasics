1bf1e6682930750b137ea0ef67e519d9de10795b
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-14 21:49:40 +0300

    100/100 judge

--- C#/04.Forloop-MoreExercises/T10_Clock-Part2/T10_Clock-Part2.cs
+++ C#/04.Forloop-MoreExercises/T10_Clock-Part2/T10_Clock-Part2.cs
@@ -1,1 +1,10 @@
-
+for (int h = 0; h < 24; h++)
+{
+    for (int min = 0; min < 60; min++)
+    {
+        for (int sec = 0; sec < 60; sec++)
+        {
+            Console.WriteLine($"{h:D2}:{min:D2}:{sec:D2}");
+        }
+    }
+}
