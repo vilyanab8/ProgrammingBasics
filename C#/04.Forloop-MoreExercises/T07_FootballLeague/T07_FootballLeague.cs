124d364cc6518940cb7dbbd79eea8daf305386c0
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-10-14 21:36:08 +0300

    100/100 judge

--- C#/04.Forloop-MoreExercises/T07_FootballLeague/T07_FootballLeague.cs
+++ C#/04.Forloop-MoreExercises/T07_FootballLeague/T07_FootballLeague.cs
@@ -1,1 +1,33 @@
+int capacity = int.Parse(Console.ReadLine());
+int fansCount = int.Parse(Console.ReadLine());
 
+int a = 0;
+int b = 0;
+int v = 0;
+int g = 0;
+
+for (int i = 0; i < fansCount; i++)
+{
+    string sector = Console.ReadLine();
+	if (sector == "A")
+	{
+		a++;
+	}
+	else if (sector == "B")
+	{
+		b++;
+	}
+	else if (sector == "V")
+	{
+		v++;
+	}
+	else if (sector == "G")
+	{
+		g++;
+	}
+}
+Console.WriteLine($"{(a * 1.0 /fansCount * 100):f2}%");
+Console.WriteLine($"{(b * 1.0 /fansCount * 100):f2}%");
+Console.WriteLine($"{(v * 1.0 /fansCount * 100):f2}%");
+Console.WriteLine($"{(g * 1.0 /fansCount * 100):f2}%");
+Console.WriteLine($"{(fansCount * 1.0 /capacity * 100):f2}%");
