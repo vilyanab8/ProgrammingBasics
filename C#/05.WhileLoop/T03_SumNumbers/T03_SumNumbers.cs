0bc48202d1e6587369ba8976ca30d40c6331772f
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-04 22:54:54 +0200

    100/100 judge

--- C#/05.WhileLoop/T03_SumNumbers/T03_SumNumbers.cs
+++ C#/05.WhileLoop/T03_SumNumbers/T03_SumNumbers.cs
@@ -1,1 +1,15 @@
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (true)
+{
+    int numbers = int.Parse(Console.ReadLine());
+    sum += numbers;
+
+    if (sum >= n)
+    {
+        Console.WriteLine(sum);
+        break;
+    }
+}
 
