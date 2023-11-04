40519dda4ef27aee98ed73b7adf8402d7b0e6d8a
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-04 23:37:16 +0200

    100/100 judge

--- C#/05.WhileLoop/T08_Graduation/T08_Graduation.cs
+++ C#/05.WhileLoop/T08_Graduation/T08_Graduation.cs
@@ -1,1 +1,28 @@
+string name = Console.ReadLine();
+int grade = 1;
+double sum = 0.0;
+int fails = 0;
 
+while (grade <= 12)
+{
+    double grades = double.Parse(Console.ReadLine());
+    if (grades >= 4)
+    {
+        sum += grades;
+        grade++;
+    }
+    else
+    {
+        fails += 1;
+
+    }
+    if (fails > 1)
+    {
+        Console.WriteLine($"{name} has been excluded at {grade} grade"); break;
+    }
+}
+if (fails < 2)
+{
+    double average = sum / 12;
+    Console.WriteLine($"{name} graduated. Average grade: {average:F2}");
+}
