c2bd2401dd02b26849ec8b746aac05303d53cbab
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-04 23:21:05 +0200

    100/100 judge

--- C#/05.WhileLoop/T05_AccountBalance/T05_AccountBalance.cs
+++ C#/05.WhileLoop/T05_AccountBalance/T05_AccountBalance.cs
@@ -1,1 +1,16 @@
+string input = Console.ReadLine();
+double balance = 0.0;
 
+while (input != "NoMoreMoney")
+{
+    double amount = double.Parse(input);
+	if (amount < 0)
+	{
+		Console.WriteLine("Invalid operation!");
+		break;
+	}
+	balance += amount;
+	Console.WriteLine($"Increase: {amount:f2}");
+	input = Console.ReadLine();
+}
+Console.WriteLine($"Total: {balance:f2}");
