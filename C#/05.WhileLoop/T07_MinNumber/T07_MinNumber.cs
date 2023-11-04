e88fe662a344fca56291135f3cb7a7ef8cce25a7
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-04 23:26:59 +0200

    100/100 judge

--- C#/05.WhileLoop/T07_MinNumber/T07_MinNumber.cs
+++ C#/05.WhileLoop/T07_MinNumber/T07_MinNumber.cs
@@ -1,1 +1,13 @@
+string input = Console.ReadLine();
+int min = int.MaxValue;
 
+while (input != "Stop")
+{
+	int numbers = int.Parse(input);
+	if (numbers < min)
+	{
+		min = numbers;
+	}
+	input = Console.ReadLine();
+}
+Console.WriteLine(min);
