8e1cffa257f0ca5ad5541da2d0684b46db81e2d4
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-04 23:25:37 +0200

    100/100 judge

--- C#/05.WhileLoop/T06_MaxNumber/T06_MaxNumber.cs
+++ C#/05.WhileLoop/T06_MaxNumber/T06_MaxNumber.cs
@@ -1,1 +1,13 @@
+string input = Console.ReadLine();
+int max = int.MinValue;
 
+while (input != "Stop")
+{
+	int numbers = int.Parse(input);
+	if (numbers > max)
+	{
+		max = numbers;
+	}
+	input = Console.ReadLine();
+}
+Console.WriteLine(max);
