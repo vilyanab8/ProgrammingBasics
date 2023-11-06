a1b6d9f2bafdfdb9310f20f2daca45d774ab686c
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-06 16:08:43 +0200

    100/100 judge

--- C#/05.WhileLoop-Exercise/T02_ExamPreparation/T02_ExamPreparation.cs
+++ C#/05.WhileLoop-Exercise/T02_ExamPreparation/T02_ExamPreparation.cs
@@ -1,1 +1,34 @@
+int failedThreshold = int.Parse(Console.ReadLine());
+int failedTimes = 0;
+int solvedProblemsCount = 0;
+double gradesSum = 0.0;
+string lastProblem = string.Empty;
+bool isFailed = true;
 
+while (failedTimes < failedThreshold)
+{
+    string problemName = Console.ReadLine();
+	if (problemName == "Enough")
+	{
+		isFailed = false;
+		break;
+	}
+	int grade = int.Parse(Console.ReadLine());
+	if (grade <= 4)
+	{
+		failedTimes++;
+	}
+	gradesSum += grade;
+	solvedProblemsCount++;
+	lastProblem = problemName;
+}
+if (isFailed)
+{
+	Console.WriteLine($"You need a break, {failedThreshold} poor grades.");
+}
+else
+{
+	Console.WriteLine($"Average score: {gradesSum / solvedProblemsCount:f2}");
+	Console.WriteLine($"Number of problems: {solvedProblemsCount}");
+	Console.WriteLine($"Las problem: {lastProblem}");
+}
