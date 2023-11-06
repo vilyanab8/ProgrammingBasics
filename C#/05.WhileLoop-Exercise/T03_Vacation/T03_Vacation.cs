ec61190ce3fa4921c757593c5513e78228b1c58c
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-06 16:29:12 +0200

    100/100 judge

--- C#/05.WhileLoop-Exercise/T03_Vacation/T03_Vacation.cs
+++ C#/05.WhileLoop-Exercise/T03_Vacation/T03_Vacation.cs
@@ -1,1 +1,36 @@
+double neededMoney = double.Parse(Console.ReadLine());
+double ownedMoney = double.Parse(Console.ReadLine());
+int daysCounter = 0;
+int spendingCounter = 0;
 
+while (ownedMoney < neededMoney && spendingCounter < 5)
+{
+    string action = Console.ReadLine();
+    double sumMoney = double.Parse(Console.ReadLine());
+    if (action == "save")
+    {
+        ownedMoney += sumMoney;
+        spendingCounter = 0;
+    }
+    else if (action == "spend")
+    {
+        if (sumMoney > ownedMoney)
+        {
+            sumMoney = ownedMoney;
+        }
+
+        ownedMoney -= sumMoney;
+        spendingCounter++;
+    }
+    daysCounter++;
+}
+
+if (spendingCounter == 5)
+{
+    Console.WriteLine("You can't save the money.");
+    Console.WriteLine(daysCounter);
+}
+if (ownedMoney >= neededMoney)
+{
+    Console.WriteLine($"You saved the money for {daysCounter} days.");
+}
