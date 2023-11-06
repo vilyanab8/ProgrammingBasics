a2d85c98eea9809945ab09ba43bdb511c19b93e3
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-06 15:51:23 +0200

    100/100 judge

--- C#/05.WhileLoop-Exercise/T01_OldBooks/T01_OldBooks.cs
+++ C#/05.WhileLoop-Exercise/T01_OldBooks/T01_OldBooks.cs
@@ -1,1 +1,20 @@
+string text = Console.ReadLine();
+string book = string.Empty;
+int count = 0;
 
+while (book != text)
+{
+	book = Console.ReadLine();
+	if (book == "No More Books")
+	{
+		Console.WriteLine("The book you search is not here!");
+		Console.WriteLine($"You checked {count} books.");
+		break;
+	}
+	else if (book == text)
+	{
+		
+		Console.WriteLine($"You checked {count} books and found it.");
+	}
+	count++;
+}
