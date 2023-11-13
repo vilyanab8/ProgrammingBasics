4fe2d3bf1ff50f1da318c78fe2bcb48bfa3bb1ef
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-13 23:33:44 +0200

    100/100 judge

--- C#/05.WhileLoop-Exercise/T07_Moving/T07_Moving.cs
+++ C#/05.WhileLoop-Exercise/T07_Moving/T07_Moving.cs
@@ -1,1 +1,23 @@
+int widthFreeSpace = int.Parse(Console.ReadLine());
+int lengthFreeSpace = int.Parse(Console.ReadLine());
+int heightFreeSpace = int.Parse(Console.ReadLine());
+string command = Console.ReadLine();
+
+double freeSpace = widthFreeSpace * lengthFreeSpace * heightFreeSpace;
+int allSpace = 0;
+
+while (command != "Done")
+{
+    double boxesCount = double.Parse(command);
+    freeSpace = freeSpace - boxesCount;
+    if (freeSpace < 0)
+    {
+        Console.WriteLine($"No more free space! You need {Math.Abs(freeSpace)} Cubic meters more.");
+        return;
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine($"{freeSpace} Cubic meters left.");           
+        
+
 
