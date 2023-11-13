99af62e08a3c48bb357f465117a32a22aec27f3f
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-13 23:20:15 +0200

    100/100 judge

--- C#/05.WhileLoop-Exercise/T06_Cake/T06_Cake.cs
+++ C#/05.WhileLoop-Exercise/T06_Cake/T06_Cake.cs
@@ -1,1 +1,23 @@
+int cakeLength = int.Parse(Console.ReadLine());
+int cakeWidth = int.Parse(Console.ReadLine());
 
+int cakeSize = cakeLength * cakeWidth;
+int cakeTaken = 0;
+string command = Console.ReadLine();
+
+while (command != "STOP")
+{
+    int currentSize = int.Parse(command);
+    cakeTaken += currentSize;
+    if (cakeTaken >= cakeSize)
+    {
+        Console.WriteLine($"No more cake left! You need {cakeTaken - cakeSize} pieces more.");
+        break;
+    }
+    command = Console.ReadLine();
+}
+
+if (cakeSize > cakeTaken)
+{
+    Console.WriteLine($"{cakeSize - cakeTaken} pieces are left.");
+}
