14d4e410c3124c8cc8635463c7d642d5026edc26
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-13 23:05:46 +0200

    100/100 judge

--- C#/05.WhileLoop-Exercise/T05_Coins/T05_Coins.cs
+++ C#/05.WhileLoop-Exercise/T05_Coins/T05_Coins.cs
@@ -1,1 +1,49 @@
+double money = double.Parse(Console.ReadLine());
+int coin = 0;
+money = Math.Round(money * 100);
 
+while (money > 0)
+{
+    if (money >= 200)
+    {
+        coin++;
+        money = money - 200;
+    }
+    else if (money >= 100)
+    {
+        coin++;
+        money = money - 100;
+    }
+    else if (money >= 50)
+    {
+        coin++;
+        money = money - 50;
+    }
+    else if (money >= 20)
+    {
+        coin++;
+        money = money - 20;
+    }
+    else if (money >= 10)
+    {
+        coin++;
+        money = money - 10;
+    }
+    else if (money >= 5)
+    {
+        coin++;
+        money = money - 5;
+    }
+    else if (money >= 2)
+    {
+        coin++;
+        money = money - 2;
+    }
+    else if (money >= 1)
+    {
+        coin++;
+        money = money - 1;
+    }
+}
+
+Console.WriteLine(coin);
