69258317a85c9b2e9d4d8ba69986a1f5ed1f8806
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-18 16:39:20 +0200

    100/100 judge

--- C#/05.WhileLoop-MoreExercises/T03_StreamOfLetters/T03_StreamOfLetters.cs
+++ C#/05.WhileLoop-MoreExercises/T03_StreamOfLetters/T03_StreamOfLetters.cs
@@ -1,1 +1,48 @@
+string command = string.Empty;
+string word = string.Empty;
+string sumSecretCommand = string.Empty;
+char currentLetter = '+';
+string newWord = string.Empty;
 
+while (command != "End")
+{
+    command = Console.ReadLine();
+    if (command != "End")
+    {
+        currentLetter = char.Parse(command);
+        if ((currentLetter >= 65 && currentLetter <= 90) || (currentLetter >= 97 && currentLetter <= 122))
+        {
+            if (currentLetter == 'c' && !(sumSecretCommand.Contains("c")))
+            {
+                sumSecretCommand += char.ToString(currentLetter);
+
+            }
+            else if (currentLetter == 'o' && !(sumSecretCommand.Contains("o")))
+            {
+                sumSecretCommand += char.ToString(currentLetter);
+            }
+            else if (currentLetter == 'n' && !(sumSecretCommand.Contains("n")))
+            {
+                sumSecretCommand += char.ToString(currentLetter);
+            }
+            else
+            {
+                word += char.ToString(currentLetter);
+            }
+            if (sumSecretCommand.Contains("c") && sumSecretCommand.Contains("o") && sumSecretCommand.Contains("n"))
+            {
+                word += " ";
+                newWord = word;
+                sumSecretCommand = "";
+            }
+        }
+    }
+    else
+    {
+        break;
+    }
+}
+if (command == "End")
+{
+    Console.WriteLine(newWord);
+}
