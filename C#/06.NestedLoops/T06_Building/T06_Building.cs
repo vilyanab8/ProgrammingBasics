c17effe246f6c197b77933dfa8fbd7fbaab2d298
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-18 18:27:13 +0200

    100/100 judge

--- C#/06.NestedLoops/T06_Building/T06_Building.cs
+++ C#/06.NestedLoops/T06_Building/T06_Building.cs
@@ -1,1 +1,22 @@
+int stagesCount = int.Parse(Console.ReadLine());
+int roomsCount = int.Parse(Console.ReadLine());
 
+for (int i = stagesCount; i > 0; i--)
+{
+    for (int j = 0; j < roomsCount; j++)
+    {
+        if (i == stagesCount)
+        {
+            Console.Write($"L{i}{j} ");
+        }
+        else if (i % 2 == 0)
+        {
+            Console.Write($"O{i}{j} ");
+        }
+        else
+        {
+            Console.Write($"A{i}{j} ");
+        }
+    }
+    Console.WriteLine();
+}
