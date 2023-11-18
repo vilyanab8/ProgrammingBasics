8c9647584c4111db5772a402f3c236abdaf7df40
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-18 18:20:42 +0200

    100/100 judge

--- C#/06.NestedLoops/T05_Travelling/T05_Travelling.cs
+++ C#/06.NestedLoops/T05_Travelling/T05_Travelling.cs
@@ -1,1 +1,15 @@
+while (true)
+{
+    string destination = Console.ReadLine();
+    if (destination == "End")
+        break;
+    double expences = double.Parse(Console.ReadLine()), savings = 0;
 
+    while (savings < expences)
+    {
+        double money = double.Parse(Console.ReadLine());
+        savings += money;
+    }
+
+    Console.WriteLine($"Going to {destination}!");
+}
