1e738908617503d1c4a86dd62b41b22dbcdbde2a
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-18 16:48:57 +0200

    100/100 judge

--- C#/05.WhileLoop-MoreExercises/T05_AverageNumber/T05_AverageNumber.cs
+++ C#/05.WhileLoop-MoreExercises/T05_AverageNumber/T05_AverageNumber.cs
@@ -1,1 +1,11 @@
+int n = int.Parse(Console.ReadLine());
+double sum = 0.0;
+double average = 0.0;
 
+for (int i = 0; i < n; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    sum += num;
+    average = sum / n;
+}
+Console.WriteLine($"{average:f2}");
