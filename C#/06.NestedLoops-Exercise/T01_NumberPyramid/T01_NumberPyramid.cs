fac36e589fe19e29b2698888aee1b65fc009b395
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-21 18:40:48 +0200

    100/100 judge

--- C#/06.NestedLoops-Exercise/T01_NumberPyramid/T01_NumberPyramid.cs
+++ C#/06.NestedLoops-Exercise/T01_NumberPyramid/T01_NumberPyramid.cs
@@ -1,1 +1,23 @@
+int n = int.Parse(Console.ReadLine());
+int curr = 1;
+bool isBigger = false;
+
+for (int i = 1; i <= n; i++)
+{
+	for (int j = 1; j <= i; j++)
+	{
+		if (curr > n)
+		{
+			isBigger = true;
+			break;
+		}
+		Console.Write(curr + " ");
+		curr++;
+	}
+	if (isBigger)
+	{
+		break;
+	}
+	Console.WriteLine();
+}
 
