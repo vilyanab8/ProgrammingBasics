8cf406a9f08c731460ce738116e9ba80f3bde68f
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-27 23:43:03 +0200

    100/100 judge

--- C#/06.NestedLoops-Exercise/T02_EqualSumsEvenOddPosition/T02_EqualSumsEvenOddPosition.cs
+++ C#/06.NestedLoops-Exercise/T02_EqualSumsEvenOddPosition/T02_EqualSumsEvenOddPosition.cs
@@ -1,1 +1,25 @@
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int evenSum = 0;
+int oddSum = 0;
 
+for (int i = num1; i <= num2; i++)
+{
+    string currNum = i.ToString();
+	for (int j = 0;	j < currNum.Length; j++)
+	{
+		int currDigit = int.Parse(currNum.ToString());
+		if (j % 2 == 0)
+		{
+			evenSum += currDigit;
+		}
+		else
+		{
+			oddSum += currDigit;
+		}
+	}
+	if (oddSum == evenSum)
+	{
+		Console.WriteLine(i + " ");
+	}
+}
