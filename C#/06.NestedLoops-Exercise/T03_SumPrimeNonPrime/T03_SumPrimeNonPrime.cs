c747dcc3b9bf04b431887986eecdda78b10fd552
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-11-27 23:55:45 +0200

    100/100 judge

--- C#/06.NestedLoops-Exercise/T03_SumPrimeNonPrime/T03_SumPrimeNonPrime.cs
+++ C#/06.NestedLoops-Exercise/T03_SumPrimeNonPrime/T03_SumPrimeNonPrime.cs
@@ -1,1 +1,43 @@
+string input = Console.ReadLine();
+int sumPrime = 0;
+int sumNonPrime = 0;
 
+while (input != "stop")
+{
+    bool isPrime = true; 
+    int n = int.Parse(input);
+    if (n < 0)
+    {
+        n = 0;
+        Console.WriteLine("Number is negative.");
+    }
+    else if (n == 1)
+    {
+        isPrime = false;
+    }
+    else
+    {
+        for (int count = 2; count <= n; count++)
+        {
+            if (n % count == 0 && count != n)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime)
+    {
+        sumPrime += n;
+    }
+    else if (isPrime == false)
+    {
+        sumNonPrime += n;
+    }
+    input = Console.ReadLine();
+}
+if (input == "stop")
+{
+    Console.WriteLine($"Sum of all prime numbers is: {sumPrime}");
+    Console.WriteLine($"Sum of all non prime numbers is: {sumNonPrime}");
+}
