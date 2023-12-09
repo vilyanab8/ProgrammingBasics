3ae8e418cca9c62e30ed85399a55637a4275261f
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-12-09 23:59:01 +0200

    100/100 judge

--- C#/06.NestedLoops-Exercise/T04_TrainTheTrainers/T04_TrainTheTrainers.cs
+++ C#/06.NestedLoops-Exercise/T04_TrainTheTrainers/T04_TrainTheTrainers.cs
@@ -1,1 +1,24 @@
+int n = int.Parse(Console.ReadLine());
+string input = Console.ReadLine();
+//double prezentationEV = 0;
+int prezentionnum = 0;
+double evaluetion = 0;
+string prezeteishenname;
+while (input != "Finish") // Finish !!!
+{
+    double prezentationEV = 0; // must be set to 0 inside of while loop
+    prezeteishenname = input;
+    for (int i = 1; i <= n; i++)
+    {
+        prezentationEV += double.Parse(Console.ReadLine());
+    }
+    prezentationEV = prezentationEV / n;
+    evaluetion += prezentationEV;
 
+    Console.WriteLine($"{prezeteishenname} - {prezentationEV:F2}.");
+    prezentionnum++;
+    input = Console.ReadLine();
+
+}
+
+Console.WriteLine($"Student's final assessment is {evaluetion / prezentionnum:F2}.");
