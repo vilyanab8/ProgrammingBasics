45d78561fc70dc9b27e198cb3c0ac77d99dec112
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-12-10 00:26:00 +0200

    100/100 judge

--- C#/06.NestedLoops-Exercise/T06_CinemaTickets/T06_CinemaTickets.cs
+++ C#/06.NestedLoops-Exercise/T06_CinemaTickets/T06_CinemaTickets.cs
@@ -1,1 +1,38 @@
+int standardTicket = 0;
+int studentTicket = 0;
+int kidTicket = 0;
+int totalSeats = 0;
 
+string movie = Console.ReadLine();
+while (movie != "Finish")
+{
+    int allSeatsMovie = int.Parse(Console.ReadLine());
+    int currentSeatsMovie = 0; //заети места!
+
+    string ticketType;
+    while (currentSeatsMovie < allSeatsMovie && (ticketType = Console.ReadLine()) != "End")
+    {
+        switch (ticketType)
+        {
+            case "standard":
+                standardTicket++;
+                break;
+            case "student":
+                studentTicket++;
+                break;
+            case "kid":
+                kidTicket++;
+                break;
+        }
+        currentSeatsMovie++;
+    }
+
+    double utilized = (double)currentSeatsMovie / (double)allSeatsMovie * 100;
+    Console.WriteLine($"{movie} - {utilized:F2}% full.");
+    totalSeats += currentSeatsMovie;
+}
+
+Console.WriteLine($"Total tickets: {totalSeats}");
+Console.WriteLine($"{((decimal)studentTicket / totalSeats * 100):F2}% student tickets.");
+Console.WriteLine($"{((decimal)standardTicket / totalSeats * 100):F2}% standard tickets.");
+Console.WriteLine($"{((decimal)kidTicket / totalSeats * 100):F2}% kids tickets.");
