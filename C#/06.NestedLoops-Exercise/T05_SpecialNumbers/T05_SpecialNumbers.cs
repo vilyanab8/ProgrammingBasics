e2732ddad8989a143bf8afebd069a39f2ba46405
V.Bozhkova <118614940+vilyanab8@users.noreply.github.com>
2023-12-10 00:17:19 +0200

    100/100 judge

--- C#/06.NestedLoops-Exercise/T05_SpecialNumbers/T05_SpecialNumbers.cs
+++ C#/06.NestedLoops-Exercise/T05_SpecialNumbers/T05_SpecialNumbers.cs
@@ -1,1 +1,32 @@
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 9; i++)
+{
+	if (n % i != 0)
+	{
+		continue;
+	}
+	for (int j = 1; j <= 9; j++)
+	{
+		if (n % j != 0)
+		{
+			continue;
+		}
+		for (int k = 1; k <= 9; k++)
+		{
+			if (n % k != 0)
+			{
+				continue;
+			}
+			for (int l = 1; l <= 9; l++)
+			{
+				if (n % l != 0)
+				{
+					continue;
+				}
+                Console.WriteLine($"{i}{j}{k}{l}");
+            }
+		}
+	}
+}
 
